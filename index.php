8cdf9bff8ab313f3e5bee3f5bb06bf781704e12f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 13:42:28 +0200

    Exo2p3 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+<p>
+<?php
+  $a = 0;
+  $b = 15;
+  while($a <= 20){
+    echo $a * $b . '<br />';
+    $a ++;
+  }
+ ?>
+</p>
+  </body>
+</html>
